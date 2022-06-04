dd558e84a23b81b262f9eafed8d69705336988fe
shiwyang <872802292@qq.com>
2022-06-04 19:21:41 +0800

    fix:remove some logger const

--- gee-web/gweb/logger.go
+++ gee-web/gweb/logger.go
@@ -5,18 +5,6 @@ import (
 	"time"
 )
 
-// TODO: add color support
-const (
-	green   = "\033[97;42m"
-	white   = "\033[90;47m"
-	yellow  = "\033[90;43m"
-	red     = "\033[97;41m"
-	blue    = "\033[97;44m"
-	magenta = "\033[97;45m"
-	cyan    = "\033[97;46m"
-	reset   = "\033[0m"
-)
-
 // Logger as a middleware example
 func Logger() HandlerFunc {
 	return func(c *Context) {
